8c498480a2873a8763c36ddc10a194817d7d1ab9
dimarog1 <dima.rogatnev123@gmail.com>
2022-10-02 23:52:18 +0300

    решение задач из презы

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
+# Задача 1
+x = int(input())
+print(x ** 2 + 3 * x - 7)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Задача 2
+for i in range(8):
+    print(2 ** i)
+
+# Задача 3
+a, b = int(input()), int(input())
+print(a ** (1 / b))
+
+# Задача 4
+s = 'djsbdhjsbdhabddsaxxahophfejfhejfhwekbekjvbjhbvhdvbjsdhvbjshbvjbdjbsjbjdbhvjdvbjsdbvjsvbjbv'
+used = ''
+ans = 0
+for i in s:
+    if i not in used:
+        ans += 1
+        used += i
+print(ans)
+
+# Задача 5
+inp = list(set(map(int, input().split())))
+print(inp[-2])
+
+# Задача 6
+def reverseString(string):
+    return string[::-1]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Задача 7
+def triangle(a, b, c):
+    if (a >= b + c) or (b >= a + c) or (c >= a + b):
+        return "Это не треугольник"
+    return "Это треугольник"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Задача 8
+def translit(string):
+    rusAlphabet = "абвгдеёжзийклмнопрстуфхцчшъыьэюя"
+    translitAlphabet = "a b v g d e yo zh z i y k l m n o" \
+                       " p r s t u f kh ts ch sh shch ` y ` e yu ia".split()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    trans = {rusAlphabet[i]: translitAlphabet[i] for i in range(len(rusAlphabet))}
+
+    res = ""
+    for elem in string:
+        if elem in trans:
+            res += trans[elem]
+        else:
+            res += elem
+
+    return res
+
+
+# Задача 9
+def convertToInt(strList):
+    return list(map(int, strList))
+
+
+# Задача 10
+def totalOrDif(set1, set2, param="and"):
+    if param == "and":
+        return set1 & set2
+    elif param == "dif":
+        return set1 ^ set2
